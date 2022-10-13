61a790ca147417b7447894044e60e11821544bd7
maxmurr <maxmurr.m@gmail.com>
2022-10-14 00:48:59 +0700

    foodAllergy crud

---
+++ app/Http/Controllers/Api/FoodAllergyController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\FoodAllergy;
+use App\Http\Resources\FoodAllergyResource;
+use App\Http\Requests\StoreFoodAllergyRequest;
+use App\Http\Requests\UpdateFoodAllergyRequest;
+
+class FoodAllergyController extends Controller
+{
+    public function index() 
+    {
+        return FoodAllergy::all();
+    }
+
+    // public function show(FoodAllergy $foodAllergy)
+    // {
+    //     return new FoodAllergyResource($foodAllergy);
+    // }
+
+    public function store(StoreFoodAllergyRequest $request)
+    {
+        return new FoodAllergyResource(FoodAllergy::create($request->all()));
+        if ($foodAllergy->save()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'FoodAllergy saved successfully',
+                'foodAllergy' => $foodAllergy->food_allergies
+            ], 200);
+        }
+        return response()->json([
+            'success' => false,
+            'message' => 'FoodAllergy saved failed'
+        ], 500);
+    }
+
+    // public function update(UpdateFoodAllergyRequest $request, FoodAllergy $foodAllergy) 
+    // {
+    //     $foodAllergy->update($request->all());
+    //     if ($foodAllergy->save()) {
+    //         return response()->json([
+    //             'success' => true,
+    //             'message' => 'FoodAllergy updated successfully',
+    //             'foodAllergy' => $foodAllergy->food_allergies
+    //         ], 200);
+    //     }
+    //     return response()->json([
+    //         'success' => false,
+    //         'message' => 'FoodAllergy updated failed'
+    //     ], 500);
+    // }
+
+    // public function destroy(FoodAllergy $foodAllergy)
+    // {
+    //     $name = $foodAllergy->food_allergy;
+    //     if ($foodAllergy->delete()) {
+    //         return response()->json([
+    //             'success' => true,
+    //             'message' => "FoodAllergy {$name} deleted successfully"
+    //         ], 200);
+    //     }
+    //     return response()->json([
+    //         'success' => false,
+    //         'message' => "FoodAllergy {$name} deleted failed"
+    //     ], 500);
+    // }
+}
