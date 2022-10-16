ac2ace524b1846a9050be158bad91920c7f9190d
maxmurr <maxmurr.m@gmail.com>
2022-10-17 02:16:53 +0700

    OrderDescription crud

---
+++ app/Http/Resources/OrderDescriptionResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderDescriptionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'food_id' => $this->food_id,
+            'order_quantity' => $this->order_quantity,
+            'order_status' => $this->order_status,
+            'order_request' => $this->order_request,
+            'order_price' => $this->order_price,
+        ];
+    }
+}
