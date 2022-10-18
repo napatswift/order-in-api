7387b41c29650c820550a5729f3b2d7c08665482
maxmurr <maxmurr.m@gmail.com>
2022-10-18 21:13:27 +0700

    api jwt

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,16 @@ Route::apiResource('tables', App\Http\Controllers\Api\TableController::class);
 Route::apiResource('orders', App\Http\Controllers\Api\OrderController::class);
 Route::apiResource('orderDescriptions', App\Http\Controllers\Api\OrderDescriptionController::class);
 Route::apiResource('reviews', App\Http\Controllers\Api\ReviewController::class);
+
+// routes/api.php
+use App\Http\Controllers\Api\AuthController;
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
