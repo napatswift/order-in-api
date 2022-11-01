cf92c369979c6055319ab24367e9df49a5161fe1
napatswift <napat.do@ku.th>
2022-11-01 16:39:00 +0700

    Fix test json

--- tests/Feature/Api/FoodTest.php
+++ tests/Feature/Api/FoodTest.php
@@ -7,6 +7,7 @@ use App\Models\Manager;
 use App\Models\Restaurant;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
@@ -41,14 +42,17 @@ class FoodTest extends TestCase
             'Authorization' => 'Bearer ' . $login_response['access_token'],
             ])->getJson(
                 '/api/foods',
-            );
+        );
 
         $response
             ->assertStatus(200)
             ->assertJson(fn (AssertableJson $json) =>
-                $json->has(10));
+                $json
+                    ->has('data', 10)
+        );
     }
-    public function test_get_all_food_with_description()
+
+    public function test_get_food_with_categories()
     {
         $user = new User();
         $user->name = fake()->name;
@@ -73,15 +77,19 @@ class FoodTest extends TestCase
             'Accept' => 'application/json',
             'Authorization' => 'Bearer ' . $login_response['access_token'],
             ])->getJson(
-                '/api/foods?description=true',
-            );
+                '/api/foods?relations=categories',
+        );
 
         $response
             ->assertStatus(200)
             ->assertJson(fn (AssertableJson $json) =>
-                $json->has(10)
-            );
+                $json
+                    ->has('data', 10)
+                    ->has('data.0.categories', 0)
+        );
     }
+
+
 
     public function test_manager_with_no_restaurant_add_food()
     {
